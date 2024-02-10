0425fd31b8df1f17a249c6d7307a4751a48dd98e
Fei Chen <boxchenfei@gmail.com>
2024-02-10 21:03:47 +0100

    Optimize the graph construction

--- src/imgdataconvertcodegen/knowledge_graph_construction/knowledge_graph_builder.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/knowledge_graph_builder.py
@@ -1,9 +1,11 @@
 import itertools
 import os.path
 import warnings
+from datetime import datetime
 
 from .knowledge_graph import KnowledgeGraph
-from ..metadata_differ import is_single_metadata_differ
+from ..metadata_differ import is_same_metadata
+from ..measure import get_execution_time
 
 
 class KnowledgeGraphBuilder:
@@ -17,33 +19,39 @@ class KnowledgeGraphBuilder:
     def knowledge_graph(self):
         return self._graph
 
-    def build(self) -> KnowledgeGraph:
-        if os.path.exists(self._know_graph_file_path):
+    def build(self, force_to_rebuild=False) -> KnowledgeGraph:
+        if not force_to_rebuild and os.path.exists(self._know_graph_file_path):
             self.build_from_file(self._know_graph_file_path)
         else:
-            warnings.warn("The knowledge graph file does not exist. The system will construct the knowledge graph"
-                          " from scratch, which may take some time..")
             self.build_from_scratch()
         print(self.knowledge_graph)
         return self.knowledge_graph
 
     def build_from_file(self, path):
+        start_time = datetime.now()
         self.knowledge_graph.load_from_file(path)
+        end_time = datetime.now()
+        print(get_execution_time(start_time, end_time))
 
     def build_from_scratch(self):
+        start_time = datetime.now()
         keys = list(self._metadata_values.keys())
         values_lists = list(self._metadata_values.values())
         for source_value in itertools.product(*values_lists):
             source_metadata = dict(zip(keys, source_value))
-            for target_value in itertools.product(*values_lists):
-                target_metadata = dict(zip(keys, target_value))
-                allow_edge_exist = is_single_metadata_differ(source_metadata, target_metadata)
-                if allow_edge_exist:
+            for attribute_name in self._metadata_values.keys():
+                for target_value in self._metadata_values[attribute_name]:
+                    target_metadata = source_metadata.copy()
+                    target_metadata[attribute_name] = target_value
+                    if is_same_metadata(source_metadata, target_metadata):
+                        continue
                     convert_function = self._create_conversion_function(source_metadata, target_metadata)
                     if convert_function is not None:
                         source_id = self.knowledge_graph.add_node(source_metadata)
                         target_id = self.knowledge_graph.add_node(target_metadata)
                         self.knowledge_graph.add_edge(source_id, target_id, conversion=convert_function)
+        end_time = datetime.now()
+        print(get_execution_time(start_time, end_time))
         self.knowledge_graph.save_to_file(self._know_graph_file_path)
 
     def _create_conversion_function(self, source, target):
@@ -56,4 +64,3 @@ class KnowledgeGraphBuilder:
     def add_new_edge_factory(self, factory):
         self._edge_factories.append(factory)
         self.build_from_scratch()
-
