afab81d6c6341aa4a0e67379afc975555472764d
Fei Chen <boxchenfei@gmail.com>
2024-02-10 19:52:56 +0100

    Simplify the edge create factory

--- src/imgdataconvertcodegen/knowledge_graph_construction/default_edges_factories.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/default_edges_factories.py
@@ -1,75 +1,44 @@
-from ..metadata_differ import is_only_this_key_differ, are_both_same_data_repr
+from ..metadata_differ import are_both_same_data_repr
 
 # NOTE: the source and target metadata are only different in one attribute
 
 
-def numpy_between_rgb_bgr(source, target):
-    def version_match():
-        return True
-
-    def metadata_match(source_metadata, target_metadata):
-        if not are_both_same_data_repr(source, target, 'numpy.ndarray'):
-            return False
-
-        if (source_metadata.get('color_channel') == 'bgr' and
-            target_metadata.get('color_channel') == 'rgb') or \
-           (source_metadata.get('color_channel') == 'rgb' and
-                target_metadata.get('color_channel') == 'bgr'):
-            return True
-
-        return False
-
-    if version_match() and metadata_match(source, target):
+def numpy_between_rgb_bgr(source_metadata, target_metadata):
+    if not are_both_same_data_repr(source_metadata, target_metadata, 'numpy.ndarray'):
+        return None
+    if ((source_metadata.get('color_channel') == 'bgr' and
+        target_metadata.get('color_channel') == 'rgb') or
+        (source_metadata.get('color_channel') == 'rgb' and
+         target_metadata.get('color_channel') == 'bgr')):
         return "def convert(var):\n  return var[:, :, ::-1]"
     return None
 
 
-def numpy_to_torch(source, target):
-    def version_match():
-        return True
-
-    def metadata_check(source_metadata, target_metadata):
-        return (source.get('data_representation') == 'numpy.ndarray' and
-                target.get('data_representation') == 'torch.tensor')
-
-    if version_match() and metadata_check(source, target):
+def numpy_to_torch(source_metadata, target_metadata):
+    if (source_metadata.get('data_representation') == 'numpy.ndarray' and
+            target_metadata.get('data_representation') == 'torch.tensor'):
         return "def convert(var):\n  return torch.from_numpy(var)"
     return None
 
 
-def torch_channel_order_last_to_first(source, target):
-    def version_match():
-        return True
-
-    def metadata_check(source_metadata, target_metadata):
-        if not are_both_same_data_repr(source, target, 'torch.tensor'):
-            return False
-
-        return (source_metadata.get('channel_order') == 'channel last' and
-                target_metadata.get('channel_order') == 'channel first')
-
-    if version_match() and metadata_check(source, target):
-        if source.get('minibatch_input') and target.get('minibatch_input'):
+def torch_channel_order_last_to_first(source_metadata, target_metadata):
+    if not are_both_same_data_repr(source_metadata, target_metadata, 'torch.tensor'):
+        return None
+    if (source_metadata.get('channel_order') == 'channel last' and
+            target_metadata.get('channel_order') == 'channel first'):
+        if source_metadata.get('minibatch_input') and target_metadata.get('minibatch_input'):
             return "def convert(var):\n  return var.permute(0, 3, 1, 2)"
         return "def convert(var):\n  return var.permute(2, 0, 1)"
     return None
 
 
-def torch_minibatch_input_false_to_true(source, target):
-    def version_match():
-        return True
+def torch_minibatch_input_false_to_true(source_metadata, target_metadata):
+    if not are_both_same_data_repr(source_metadata, target_metadata, 'torch.tensor'):
+        return None
 
-    def metadata_check(source_metadata, target_metadata):
-        if not are_both_same_data_repr(source, target, 'torch.tensor'):
-            return False
+    if (not source_metadata.get('minibatch_input')) and target_metadata.get('minibatch_input'):
+        "def convert(var):\n  return torch.unsqueeze(var, 0)"
 
-        if not is_only_this_key_differ(source_metadata, target_metadata, 'minibatch_input'):
-            return False
-
-        return (not source_metadata.get('minibatch_input')) and target_metadata.get('minibatch_input')
-
-    if version_match() and metadata_check(source, target):
-        return "def convert(var):\n  return torch.unsqueeze(var, 0)"
     return None
 
 
