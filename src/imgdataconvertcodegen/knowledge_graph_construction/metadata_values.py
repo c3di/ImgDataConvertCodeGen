d2a536bcaa05bc360588f8fde62b3c55cf4cc728
Fei Chen <boxchenfei@gmail.com>
2024-02-12 10:50:36 +0100

    Remove the data range in the metadata_values

--- src/imgdataconvertcodegen/knowledge_graph_construction/metadata_values.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/metadata_values.py
@@ -1,10 +1,11 @@
+# when data type is float, the data range is 0-1
 metadata_values = {
     "data_representation": ["torch.tensor", "numpy.ndarray", "PIL.Image", "tf.tensor"],
     "color_channel": ['rgb', 'bgr', 'gray', 'rgba', 'graya'],
     "channel_order": ['channel last', 'channel first', 'none'],
     "minibatch_input": [True, False],
-    "data_type": ['uint8', 'uint16', 'uint32', 'float32', 'int8', 'int16', 'int32', 'int64', 'double'],
-    "intensity_range": ['0to255', '0to1', '-1to1'],
+    "data_type": ['uint8', 'uint16', 'uint32', 'uint64', 'float32', 'int8',
+                  'float64', 'double', 'int16', 'int32', 'int64'],
     "device": ['cpu', 'gpu']
 }
 
