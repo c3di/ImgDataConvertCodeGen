44cee46562ace683b4ab791ae740f646a05b861b
Fei Chen <boxchenfei@gmail.com>
2024-02-17 22:55:27 +0100

    Add unit tests (#10)

--- tests/test_function_util.py
+++ tests/test_function_util.py
@@ -1,6 +1,6 @@
 import re
 from unittest.mock import patch
-from src.imgdataconvertcodegen.function_util import create_unique_function
+from src.imgdataconvertcodegen.function_util import create_unique_function, extract_func_body
 
 
 def test_create_unique_function_name_replacement():
@@ -27,3 +27,25 @@ def original_function_name(a, b):
         pattern = rf'def\s+{expected_new_name}\('
         assert re.search(pattern, new_function_definition), "New function name not found in the function definition"
         assert "return a + b" in new_function_definition, "The body of the original function has been altered"
+
+
+def test_remove_intermediate_functon_call():
+    code_str = """
+def convert(var):
+    # transformation
+    var_with_var_in_name = var + 1
+    for i in range(len(var)):
+        var[i] = var[i] * 2  # Indentation inside the loop
+    return var
+    """
+
+    expected_output = """# transformation
+var_with_var_in_name = source_image + 1
+for i in range(len(source_image)):
+    source_image[i] = source_image[i] * 2  # Indentation inside the loop
+target_image = source_image
+    """.strip()
+
+    actual = extract_func_body(code_str, 'source_image', 'target_image').strip()
+
+    assert actual == expected_output, "The transformed code does not match the expected output."
