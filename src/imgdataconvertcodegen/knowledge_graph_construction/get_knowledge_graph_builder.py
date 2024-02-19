829fb716331be3c82bfbfb73cd80feab74b54fb2
Fei Chen <boxchenfei@gmail.com>
2024-02-19 13:05:27 +0100

    Add development env (#31)

--- src/imgdataconvertcodegen/knowledge_graph_construction/get_knowledge_graph_builder.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/get_knowledge_graph_builder.py
@@ -1,10 +1,15 @@
+import os
+
 from src.imgdataconvertcodegen.knowledge_graph_construction.edge_factories import all_edge_factories
 from .metadata_values import metadata_values
 from .lib_presets import lib_presets
 from .knowledge_graph_builder import KnowledgeGraphBuilder
 
+development_mode = os.getenv('Development')
+
 builder = KnowledgeGraphBuilder(metadata_values, all_edge_factories, lib_presets)
-builder.build()
+#todo add a way to set development mode from outside
+builder.build(development_mode is None)
 
 
 def get_knowledge_graph_builder():
