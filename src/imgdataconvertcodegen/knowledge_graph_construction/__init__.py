96cf7e2663fbf343b4b21109673139e3f309d19e
Fei Chen <boxchenfei@gmail.com>
2024-03-05 10:27:18 +0100

    Optimize the import

--- src/imgdataconvertcodegen/knowledge_graph_construction/__init__.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/__init__.py
@@ -4,3 +4,4 @@ from .metadata import (encode_metadata, decode_metadata, check_metadata_value_va
 from .edge_factories import all_edge_factories
 from .knowledge_graph import KnowledgeGraph
 from .knowledge_graph_builder import KnowledgeGraphBuilder
+from .get_knowledge_graph_builder import get_knowledge_graph_builder
