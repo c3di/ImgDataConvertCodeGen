0778136e685502d48f6952cab8edb61b11cccd14
Luojinxuan Wang <luwa02@dfki.de>
2024-03-07 15:30:01 +0100

    img repr done, could still be invalid for float/float32

--- src/imgdataconvertcodegen/knowledge_graph_construction/metadata.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/metadata.py
@@ -58,26 +58,41 @@ class ValuesOfImgRepr(TypedDict):
 
 bunch_of_img_repr: dict[ImageRepr, ValuesOfImgRepr] = {
     "torch.tensor": {
-        "color_channel": ['rgb', 'bgr', 'gray', 'rgba', 'graya'],
-        "channel_order": ['channel last', 'channel first', 'none'],
+        "color_channel": ['rgb', 'gray'],
+        "channel_order": ['channel first'],
         "minibatch_input": [True, False],
-        "data_type": ['uint8', 'uint16', 'uint32', 'uint64',
-                      'float32', 'float64', 'double',
+        "data_type": ['uint8','float32', 'float64', 'double',
                       'int8', 'int16', 'int32', 'int64'],
-        "intensity_range": ['full', 'normalized_unsigned', 'normalized_signed'],
+        "intensity_range": ['full', 'normalized_unsigned'],
         "device": ['cpu', 'gpu']
     },
     "numpy.ndarray": {
         "color_channel": ['rgb', 'bgr', 'gray'],
-        "channel_order": ['channel last', 'channel first', 'none'],
-        "minibatch_input": [True, False],
-        "data_type": ['uint8', 'uint16', 'uint32', 'uint64',
-                      'float32', 'float64', 'double',
-                      'int8', 'int16', 'int32', 'int64'],
+        "channel_order": ['channel last', 'none'],
+        "minibatch_input": [False],
+        "data_type": ['uint8', 'uint16', 'uint32',
+                      'float', 'int8', 'int16', 'int32'],
         "intensity_range": ['full', 'normalized_unsigned', 'normalized_signed'],
         "device": ['cpu']
+    },
+    "PIL.Image": {
+        "color_channel": ['rgb', 'gray', 'rgba', 'graya'],
+        "channel_order": ['channel last', 'none'],
+        "minibatch_input": [False],
+        "data_type": ['uint8'],
+        "intensity_range": ['full'],
+        "device": ['cpu']
+    },
+    "tf.tensor": {
+        "color_channel": ['rgb', 'gray'],
+        "channel_order": ['channel last'],
+        "minibatch_input": [True, False],
+        "data_type": ['uint8', 'uint16', 'uint32', 'uint64',
+                  'float16', 'float32', 'float64',
+                  'int8', 'int16', 'int32', 'int64'],
+        "intensity_range": ['full', 'normalized_unsigned'],
+        "device": ['cpu', 'gpu']
     }
-    # todo: add other data representations and values
 }
 
 
