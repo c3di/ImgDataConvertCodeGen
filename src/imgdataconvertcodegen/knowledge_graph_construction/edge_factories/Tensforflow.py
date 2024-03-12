91cc601e1c75118d10ad488f75469a7372082202
Fei Chen <boxchenfei@gmail.com>
2024-03-12 17:36:14 +0100

    Cleanup the factories used for data representation in tensorflow

--- src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/Tensforflow.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/Tensforflow.py
@@ -2,60 +2,101 @@ from .type import conversion
 from ...metadata_differ import are_both_same_data_repr, is_differ_value_for_key
 
 
-def tf_gpu_cpu(source_metadata, target_metadata) -> conversion:
-    if not are_both_same_data_repr(source_metadata, target_metadata, "tf.tensor"):
-        return None
-    if (
-            source_metadata.get("device") == "gpu"
-            and target_metadata.get("device") == "cpu"
-    ):
-        if is_differ_value_for_key(source_metadata, target_metadata, "device"):
-            return ("import tensorflow as tf",
-                    "def convert(var):\n  return tf.device('/cpu:0')(var)")
-    if (
-            source_metadata.get("device") == "cpu"
-            and target_metadata.get("device") == "gpu"
-    ):
-        if is_differ_value_for_key(source_metadata, target_metadata, "device"):
-            return (
-                "import tensorflow as tf",
+def is_valid_metadata_for_tensorflow(metadata):
+    # TODO: Add valid check for combination of values for different attributes
+
+    # Todo: Add valid check for each attribute
+    allowed_values = {
+        "color_channel": ['rgb', 'gray'],
+        "channel_order": ['channel first', 'channel last', 'none'],
+        "minibatch_input": [True, False],
+        "data_type": ['uint8',
+                      'float32', 'float64', 'double',
+                      'int8', 'int16', 'int32', 'int64'],
+        "intensity_range": ['full', '-1to1'],
+        "device": ['cpu', 'gpu']
+    }
+    for key, values in allowed_values.items():
+        if key in metadata and metadata[key] not in values:
+            return False
+    return True
+
+
+def use_factories_in_cluster(source_metadata, target_metadata):
+    return (are_both_same_data_repr(source_metadata, target_metadata, "tf.tensor") and
+            is_valid_metadata_for_tensorflow(source_metadata) and
+            is_valid_metadata_for_tensorflow(target_metadata))
+
+
+def gpu_to_cpu(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("device") == "gpu" and target_metadata.get("device") == "cpu":
+        return "import tensorflow as tf", "def convert(var):\n  return tf.device('/cpu:0')(var)"
+    return None
+
+
+def cpu_to_gpu(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("device") == "cpu" and target_metadata.get("device") == "gpu":
+        return ("import tensorflow as tf",
                 """def convert(var):
-                    if not tf.config.list_physical_devices('GPU'):
-                        raise RuntimeError("GPU device not available.")
                     with tf.device('/device:GPU:0'):
-                        return tf.identity(var)""",
-            )
+                        return tf.identity(var)"""),
     return None
 
 
-def tf_convert_minibatch(source_metadata, target_metadata) -> conversion:
-    if not are_both_same_data_repr(source_metadata, target_metadata, "tf.tensor"):
-        return None
-
-    if source_metadata.get("minibatch_input") and not target_metadata.get(
-            "minibatch_input"
-    ):
-        return (
-            "import tensorflow as tf",
-            "def convert(var):\n  return tf.squeeze(var, 0)",
-        )
-
-    if not source_metadata.get("minibatch_input") and target_metadata.get(
-            "minibatch_input"
-    ):
-        return (
-            "import tensorflow as tf",
-            "def convert(var):\n  return tf.expand_dims(var, 0)",
-        )
-
+def minibatch_true_to_false(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("minibatch_input") and not target_metadata.get("minibatch_input"):
+        return "import tensorflow as tf", "def convert(var):\n  return tf.squeeze(var, 0)"
     return None
 
 
-def tf_convert_dtype(source_metadata, target_metadata) -> conversion:
-    if not are_both_same_data_repr(source_metadata, target_metadata, "tf.tensor"):
-        return None
+def minibatch_false_to_true(source_metadata, target_metadata) -> conversion:
+    if (not source_metadata.get('minibatch_input')) and target_metadata.get('minibatch_input'):
+        return "import tensorflow as tf", "def convert(var):\n  return tf.expand_dims(var, 0)"
+
+
+def channel_none_to_channel_first(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("channel_order") == "none" and target_metadata.get("channel_order") == "channel first":
+        return "import tensorflow as tf", "def convert(var):\n  return tf.expand_dims(var, 0)"
+
+
+def channel_none_to_channel_last(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("channel_order") == "none" and target_metadata.get("channel_order") == "channel last":
+        return "import tensorflow as tf", "def convert(var):\n  return tf.expand_dims(var, -1)"
+    return None
+
+
+def channel_last_to_none(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("channel_order") == "channel last" and target_metadata.get("channel_order") == "none":
+        return "import tensorflow as tf", "def convert(var):\n  return tf.squeeze(var, -1)"
+    return None
+
+
+def channel_first_to_none(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("channel_order") == "channel first" and target_metadata.get("channel_order") == "none":
+        return "import tensorflow as tf", "def convert(var):\n  return tf.squeeze(var, 0)"
+    return None
+
+
+def channel_last_to_channel_first(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("channel_order") == "channel last" and target_metadata.get(
+            "channel_order") == "channel first":
+        if source_metadata.get("minibatch_input"):
+            return "import tensorflow as tf", "def convert(var):\n  return tf.transpose(var, [0, 3, 1, 2])"
+        return "import tensorflow as tf", "def convert(var):\n  return tf.transpose(var, [2, 0, 1])"
+    return None
+
+
+def channel_first_to_channel_last(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get('channel_order') == 'channel first' and target_metadata.get(
+            'channel_order') == 'channel last':
+        if source_metadata.get('minibatch_input'):
+            return "import tensorflow as tf", "def convert(var):\n  return tf.transpose(var, [0, 2, 3, 1])"
+        return "import tensorflow as tf", "def convert(var):\n   return tf.transpose(var, [1, 2, 0])"
+    return None
+
+
+def convert_dtype(source_metadata, target_metadata) -> conversion:
     if is_differ_value_for_key(source_metadata, target_metadata, "data_type"):
-        target_dtype_str = target_metadata.get("data_type")
         dtype_mapping = {
             "uint8": "tf.uint8",
             "uint16": "tf.uint16",
@@ -70,28 +111,38 @@ def tf_convert_dtype(source_metadata, target_metadata) -> conversion:
             "int64": "tf.int64",
             "double": "tf.float64",
         }
-        target_dtype = dtype_mapping.get(target_dtype_str, None)
-        if target_dtype is None:
-            raise ValueError(f"Unsupported target dtype: {target_dtype_str}")
-
-        return (
-            "import tensorflow as tf",
-            f"""def convert(var):
-                    if '{target_dtype}' == 'tf.float32':
-                        if var.dtype.is_integer:
-                            max_val = tf.cast(tf.reduce_max(var), tf.float32)
-                            return tf.cast(var, tf.float32) / max_val
-                        else:
-                            return tf.cast(var, tf.float32)
-                    else:
-                    
-                        return tf.cast(var, {target_dtype})""",
-        )
+        target_dtype = dtype_mapping.get(target_metadata.get("data_type"))
+        return "import tensorflow as tf", f"def convert(var):\n  return tf.cast(var, {target_dtype})"
     return None
 
 
-tensorflow_factories = [
-    tf_gpu_cpu,
-    tf_convert_minibatch,
-    tf_convert_dtype,
-]
+def uint8_data_range_to_normalize(source_metadata, target_metadata) -> conversion:
+    if (source_metadata.get('data_type') == 'uint8' and
+            source_metadata.get("intensity_range") == "full" and
+            target_metadata.get("intensity_range") == "0to1"):
+        return "", "def convert(var):\n  return var / 255"
+
+
+def uint8_normalize_to_full_data_range(source_metadata, target_metadata) -> conversion:
+    if (source_metadata.get('data_type') == 'uint8' and
+            source_metadata.get("intensity_range") == "0to1" and
+            target_metadata.get("intensity_range") == "full"):
+        return "", "def convert(var):\n  return var * 255"
+    return None
+
+
+factories_cluster_for_tensorflow = (use_factories_in_cluster, [
+    gpu_to_cpu,
+    cpu_to_gpu,
+    channel_none_to_channel_first,
+    channel_none_to_channel_last,
+    channel_first_to_none,
+    channel_first_to_channel_last,
+    channel_last_to_none,
+    channel_last_to_channel_first,
+    minibatch_true_to_false,
+    minibatch_false_to_true,
+    convert_dtype,
+    uint8_data_range_to_normalize,
+    uint8_normalize_to_full_data_range,
+])
--- src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/__init__.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/__init__.py
@@ -1,8 +1,13 @@
 from .type import conversion
 from .default_edges_factories import default_factories
-from .Pytorch import pytorch_factories
+from .Pytorch import factories_cluster_for_Pytorch
 from .PIL import pil_factories
-from .Tensforflow import tensorflow_factories
+from .Tensforflow import factories_cluster_for_tensorflow
 from .inter_libs import inter_libs_factories
 
-all_edge_factories = default_factories + pil_factories + pytorch_factories + tensorflow_factories + inter_libs_factories
+all_edge_factories = default_factories + pil_factories + inter_libs_factories
+
+factories_cluster = [
+    factories_cluster_for_Pytorch,
+    factories_cluster_for_tensorflow,
+]
