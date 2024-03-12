5a08015f598f75411d4d83694ebc9127a8a635e6
Fei Chen <boxchenfei@gmail.com>
2024-03-12 11:16:54 +0100

    Add TODO tasks in inter-conversion from diversity libraries

--- src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/inter_libs.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/inter_libs.py
@@ -1,5 +1,4 @@
 from .type import conversion
-from ...metadata_differ import is_differ_value_for_key
 
 
 def pil_to_torch(source_metadata, target_metadata) -> conversion:
@@ -7,26 +6,52 @@ def pil_to_torch(source_metadata, target_metadata) -> conversion:
             source_metadata.get("data_representation") == "PIL.Image"
             and target_metadata.get("data_representation") == "torch.tensor"
     ):
-        if is_differ_value_for_key(source_metadata, target_metadata, "data_representation"):
-            common_constraints = {
-                "color_channel": ['rgb', 'gray'],
-                "channel_order": ['channel last', 'channel first', 'none'],
-                "minibatch_input": [False],
-                "data_type": ['uint8'],
-                "device": ['cpu']
-            }
+        # todo: https://pytorch.org/vision/main/generated/torchvision.transforms.functional.pil_to_tensor.html
+        common_constraints = {
+            "color_channel": ['rgb', 'gray'],
+            "minibatch_input": [False],
+            "data_type": ['uint8'],
+            "device": ['cpu']
+        }
 
-            for key, allowed_values in common_constraints.items():
-                if not source_metadata.get(key) in allowed_values:
-                    return None
+        for key, allowed_values in common_constraints.items():
+            if not source_metadata.get(key) in allowed_values:
+                return None
 
-            return (
-                "from torchvision.transforms import ToTensor",
-                "def convert(var):\n  return ToTensor()(var)",
-            )
+        return (
+            "from torchvision.transforms import ToTensor",
+            "def convert(var):\n  return ToTensor()(var)",
+        )
         return None
 
 
+# TODO: https://pytorch.org/vision/master/generated/torchvision.transforms.functional.to_pil_image.html
+def torch_to_pil(source_metadata, target_metadata) -> conversion:
+    if (
+            source_metadata.get("data_representation") == "torch.tensor"
+            and target_metadata.get("data_representation") == "PIL.Image"
+    ):
+
+        common_constraints = {
+            "color_channel": ['rgb', 'gray'],
+            "channel_order": ['channel last', 'channel first', 'none'],
+            "minibatch_input": [False],
+            "data_type": ['uint8'],
+            "device": ['cpu']
+        }
+
+        for key, allowed_values in common_constraints.items():
+            if not source_metadata.get(key) in allowed_values:
+                return None
+        return (
+            "from torchvision.transforms import ToPILImage",
+            "def convert(var):\n  return ToPILImage()(var)",
+        )
+    return None
+
+
+# TODO: split to  PIL to numpy and numpy to tensor
+# GPU？
 def pil_to_tf(source_metadata, target_metadata) -> conversion:
     if (
             source_metadata.get("data_representation") == "PIL.Image"
@@ -41,42 +66,8 @@ def pil_to_tf(source_metadata, target_metadata) -> conversion:
     return None
 
 
-def torch_to_pil(source_metadata, target_metadata) -> conversion:
-    if (
-            source_metadata.get("data_representation") == "torch.tensor"
-            and target_metadata.get("data_representation") == "PIL.Image"
-    ):
-        if is_differ_value_for_key(source_metadata, target_metadata, "data_representation"):
-            common_constraints = {
-                "color_channel": ['rgb', 'gray'],
-                "channel_order": ['channel last', 'channel first', 'none'],
-                "minibatch_input": [False],
-                "data_type": ['uint8'],
-                "device": ['cpu']
-            }
-
-            for key, allowed_values in common_constraints.items():
-                if not source_metadata.get(key) in allowed_values:
-                    return None
-        return (
-            "from torchvision.transforms import ToPILImage",
-            "def convert(var):\n  return ToPILImage()(var)",
-        )
-    return None
-
-
-def torch_to_tf(source_metadata, target_metadata) -> conversion:
-    if (
-            source_metadata.get("data_representation") == "torch.tensor"
-            and target_metadata.get("data_representation") == "tf.tensor"
-    ):
-        return (
-            "import tensorflow as tf",
-            "def convert(var):\n  return tf.convert_to_tensor(var.numpy(), dtype=tf.as_dtype(var.dtype))",
-        )
-    return None
-
-
+# TODO: split to tf-to-numpy and numpy-to-tensor
+# GPU？
 def tf_to_pil(source_metadata, target_metadata) -> conversion:
     if (
             source_metadata.get("data_representation") == "tf.tensor"
@@ -89,6 +80,21 @@ def tf_to_pil(source_metadata, target_metadata) -> conversion:
     return None
 
 
+# TODO: split to numpy-to-tf and tf-to-torch
+# GPU？
+def torch_to_tf(source_metadata, target_metadata) -> conversion:
+    if (
+            source_metadata.get("data_representation") == "torch.tensor"
+            and target_metadata.get("data_representation") == "tf.tensor"
+    ):
+        return (
+            "import tensorflow as tf",
+            "def convert(var):\n  return tf.convert_to_tensor(var.numpy(), dtype=tf.as_dtype(var.dtype))",
+        )
+    return None
+
+
+# TODO: numpy-to-torch and torch-to-numpy
 def tf_to_torch(source_metadata, target_metadata) -> conversion:
     if (
             source_metadata.get("data_representation") == "tf.tensor"
