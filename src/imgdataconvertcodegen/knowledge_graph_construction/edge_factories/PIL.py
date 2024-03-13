627c357f8a98f681f0a6343c0243bdadda582c6d
Fei Chen <boxchenfei@gmail.com>
2024-03-13 21:23:56 +0100

    Cleanup PIL and introduce factories_for_metadata_pair

--- src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/PIL.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/PIL.py
@@ -1,67 +1,124 @@
 from .type import conversion
-from ...metadata_differ import is_differ_value_for_key, are_both_same_data_repr
+from ...metadata_differ import are_both_same_data_repr
 
 
-def validate_pil(metadata):
-    pil_constraints = {
+def is_attribute_value_valid_for_pil(metadata):
+    # https://pillow.readthedocs.io/en/stable/handbook/concepts.html
+    allowed_values = {
         "color_channel": ['rgb', 'gray', 'rgba', 'graya'],
         "channel_order": ['channel last', 'none'],
         "minibatch_input": [False],
+        # Pillow doesn’t yet support multichannel images with a depth of more than 8 bits per channel.
         "data_type": ['uint8'],
-        "intensity_range": ['full'],
         "device": ['cpu'],
     }
-    if metadata['channel_order'] == 'none' and metadata['color_channel'] != 'gray':
-        return False
-    for key, allowed_values in pil_constraints.items():
+    for key, allowed_values in allowed_values.items():
         if key in metadata and metadata[key] not in allowed_values:
             return False
     return True
 
 
-def pil_rgba_to_rgb(source_metadata, target_metadata) -> conversion:
-    if are_both_same_data_repr(source_metadata, target_metadata, "PIL.Image"):
-        if validate_pil(source_metadata) and validate_pil(target_metadata):
-            if (
-                    source_metadata.get("color_channel") == "rgba"
-                    and target_metadata.get("color_channel") == "rgb"
-            ) or (
-                    source_metadata.get("color_channel") == "graya"
-                    and target_metadata.get("color_channel") == "gray"
-            ):
-                if is_differ_value_for_key(source_metadata, target_metadata, "color_channel"):
-                    return (
-                        "",
-                        """def convert(var):
-                            return var.convert("RGB") if var.mode == "RGBA" else var.convert("L")""",
-                    )
+def is_metadata_valid_for_pil(metadata):
+    if metadata['color_channel'] == 'gray' and metadata['channel_order'] != 'none':
+        return False
+    if metadata['color_channel'] != 'gray' and metadata['channel_order'] == 'none':
+        return False
+    return True
+
+
+def can_use_factories_in_cluster(source_metadata, target_metadata):
+    return (
+            are_both_same_data_repr(source_metadata, target_metadata, "PIL.Image")
+            and is_attribute_value_valid_for_pil(source_metadata)
+            and is_attribute_value_valid_for_pil(target_metadata)
+            and is_metadata_valid_for_pil(source_metadata)
+            and is_metadata_valid_for_pil(target_metadata)
+    )
+
+
+def rgba_to_rgb(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("color_channel") == "rgba" and target_metadata.get("color_channel") == "rgb":
+        return "", 'def convert(var):\n  return var.convert("RGB")'
     return None
 
 
-def pil_convert_dtype(source_metadata, target_metadata) -> conversion:
-    if are_both_same_data_repr(source_metadata, target_metadata, "PIL.Image") and is_differ_value_for_key(
-            source_metadata, target_metadata, "data_type"):
-        target_dtype = target_metadata.get("data_type")
-        if target_dtype in ["uint8", "uint16", "float32", "int32"]:
-            return (
-                "import numpy as np\nfrom PIL import Image",
-                f"""def convert(var):
-                        array = np.array(var)
-                        if '{target_dtype}' == 'uint8':
-                            converted = (array / 255.0 if array.dtype == np.float32 else array / 65535 if array.dtype == np.uint16 else array).astype(np.uint8)
-                        elif '{target_dtype}' == 'uint16':
-                            converted = (array * 65535 if array.dtype == np.uint8 else array).astype(np.uint16)
-                        elif '{target_dtype}' == 'float32':
-                            converted = (array / 255.0 if array.dtype == np.uint8 else array / 65535 if array.dtype == np.uint16 else array).astype(np.float32)
-                        elif '{target_dtype}' == 'int32':
-                            converted = array.astype(np.int32)
-    
-                        return Image.fromarray(converted) if '{target_dtype}' in ['uint8', 'uint16'] else converted""",
-            )
+def rgba_to_graya(source_metadata, target_metadata) -> conversion:
+    if source_metadata.get("color_channel") == "rgba" and target_metadata.get("color_channel") == "graya":
+        return '', 'def convert(var):\n  return var.convert("LA")'
     return None
 
 
-pil_factories = [
-    pil_rgba_to_rgb,
-    pil_convert_dtype,
+factories_cluster_for_pil = (
+    can_use_factories_in_cluster,
+    [
+        rgba_to_rgb,
+        rgba_to_graya,
+    ],
+)
+
+pil_graya_or_rgba_rgb_to_gray = ('', 'def convert(var):\n  return var.convert("L")')
+
+pil_gray_to_rgb = ('', 'def convert(var):\n  return var.convert("RGB")')
+
+factories_for_pil_metadata_pair = [
+    (
+        {
+            "color_channel": 'rgb',
+            "channel_order": 'channel last',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        },
+        {
+            "color_channel": 'gray',
+            "channel_order": 'none',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        }, pil_graya_or_rgba_rgb_to_gray),
+    (
+        {
+            "color_channel": 'rgba',
+            "channel_order": 'channel last',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        },
+        {
+            "color_channel": 'gray',
+            "channel_order": 'none',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        }, pil_graya_or_rgba_rgb_to_gray),
+    (
+        {
+            "color_channel": 'graya',
+            "channel_order": 'channel last',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        },
+        {
+            "color_channel": 'gray',
+            "channel_order": 'none',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        }, pil_graya_or_rgba_rgb_to_gray),
+    (
+        {
+            "color_channel": 'gray',
+            "channel_order": 'none',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        },
+        {
+            "color_channel": 'rgb',
+            "channel_order": 'channel last',
+            "minibatch_input": False,
+            "data_type": 'uint8',
+            "device": 'cpu',
+        }, pil_gray_to_rgb),
 ]
--- src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/__init__.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/edge_factories/__init__.py
@@ -1,18 +1,19 @@
 from .type import conversion
-from .PIL import factories_cluster_for_pil
+from .PIL import factories_cluster_for_pil, factories_for_pil_metadata_pair
+from .numpy import factories_cluster_for_numpy
 from .Pytorch import factories_cluster_for_Pytorch
 from .Tensorflow import factories_cluster_for_tensorflow
 from .inter_libs import (factories_cluster_for_numpy_pil, factories_cluster_for_numpy_torch,
                          factories_cluster_for_numpy_tensorflow)
 
-#Todo: remove and change in the knowledge graph construction
-all_edge_factories = []
-
 factories_cluster = [
     factories_cluster_for_pil,
+    factories_cluster_for_numpy,
     factories_cluster_for_Pytorch,
     factories_cluster_for_tensorflow,
     factories_cluster_for_numpy_pil,
     factories_cluster_for_numpy_torch,
     factories_cluster_for_numpy_tensorflow,
 ]
+
+factories_for_metadata_pair = factories_for_pil_metadata_pair
