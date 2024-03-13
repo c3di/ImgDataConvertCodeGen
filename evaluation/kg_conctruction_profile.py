56d532a04fb941f7dc5e42cf2a60473bc6b6f3b7
Fei Chen <boxchenfei@gmail.com>
2024-03-13 22:42:48 +0100

    Update the construction profile

--- evaluation/kg_conctruction_profile.py
+++ evaluation/kg_conctruction_profile.py
@@ -1,14 +1,18 @@
 import cProfile
 import pstats
 
-from imgdataconvertcodegen.knowledge_graph_construction import (metadata_values, all_edge_factories,
+from imgdataconvertcodegen.knowledge_graph_construction import (metadata_values, factories_clusters,
+                                                                list_of_conversion_for_metadata_pair,
                                                                 KnowledgeGraphConstructor)
 
 if __name__ == "__main__":
-    constructor = KnowledgeGraphConstructor(metadata_values, all_edge_factories)
+    print(f"{len(factories_clusters)} factories clusters, "
+          f"{sum([len(cluster[1]) for cluster in factories_clusters])} factories in total, ",
+          f"{len(list_of_conversion_for_metadata_pair)} manual edge creation.")
+    constructor = KnowledgeGraphConstructor(metadata_values, factories_clusters, list_of_conversion_for_metadata_pair)
     with cProfile.Profile() as profile:
         constructor.build_from_scratch()
-        print(constructor.knowledge_graph)
         results = pstats.Stats(profile)
         results.sort_stats(pstats.SortKey.TIME)
+        print(constructor.knowledge_graph)
         results.print_stats()
