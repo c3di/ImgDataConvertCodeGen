80d8afd7ce6980de4ecd1ec17cb7c82c9257fae5
Fei Chen <boxchenfei@gmail.com>
2024-03-13 09:36:03 +0100

    Move file

--- src/imgdataconvertcodegen/knowledge_graph_construction/metedata/__init__.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/metedata/__init__.py
@@ -1,3 +1,16 @@
-from .type import *
+from .type import MetadataValues, Metadata
 from .util import *
 
+
+metadata_values: MetadataValues = {
+    "data_representation": ["torch.tensor", "numpy.ndarray", "PIL.Image", "tf.tensor"],
+    "color_channel": ['rgb', 'bgr', 'gray', 'rgba', 'graya'],
+    "channel_order": ['channel last', 'channel first', 'none'],
+    "minibatch_input": [True, False],
+    "data_type": ['uint8', 'uint16', 'uint32', 'uint64',
+                  'float16', 'float32', 'float64', 'double',
+                  'int8', 'int16', 'int32', 'int64'],
+    # intensity_range can be also -1 to 1 or 0 to 1, https://scikit-image.org/docs/stable/user_guide/data_types.html
+    "intensity_range": ['full', '0to1', '-1to1'],
+    "device": ['cpu', 'gpu']
+}
--- src/imgdataconvertcodegen/knowledge_graph_construction/metedata/type.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/metedata/type.py
@@ -11,19 +11,6 @@ class MetadataValues(TypedDict):
     device: list[str]
 
 
-metadata_values: MetadataValues = {
-    "data_representation": ["torch.tensor", "numpy.ndarray", "PIL.Image", "tf.tensor"],
-    "color_channel": ['rgb', 'bgr', 'gray', 'rgba', 'graya'],
-    "channel_order": ['channel last', 'channel first', 'none'],
-    "minibatch_input": [True, False],
-    "data_type": ['uint8', 'uint16', 'uint32', 'uint64',
-                  'float16', 'float32', 'float64', 'double',
-                  'int8', 'int16', 'int32', 'int64'],
-    "intensity_range": ['full', '0to1', '-1to1'],
-    "device": ['cpu', 'gpu']
-}
-
-
 class Metadata(TypedDict):
     data_representation: str
     color_channel: str
