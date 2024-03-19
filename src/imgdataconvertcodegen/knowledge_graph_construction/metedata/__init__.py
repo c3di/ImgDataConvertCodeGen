6ce7edda6b26fdf6517a47b7a9dda227f00d8980
Fei Chen <boxchenfei@gmail.com>
2024-03-19 22:06:24 +0100

    format

--- src/imgdataconvertcodegen/knowledge_graph_construction/metedata/__init__.py
+++ src/imgdataconvertcodegen/knowledge_graph_construction/metedata/__init__.py
@@ -11,8 +11,9 @@ metadata_values: MetadataValues = {
                         'int8', 'int16', 'int32', 'int64'
                         # intensity range for floating point can be -1 to 1 or 0 to 1 from
                         # https://scikit-image.org/docs/stable/user_guide/data_types.html
-                        'float32(0to1)', 'float32(-1to1)', 'float64(0to1)', 'float64(-1to1)', 'double(0to1)',
-                        'double(-1to1)',
+                        'float32(0to1)', 'float32(-1to1)',
+                        'float64(0to1)', 'float64(-1to1)',
+                        'double(0to1)', 'double(-1to1)',
                         ],
     "device": ['cpu', 'gpu']
 }
