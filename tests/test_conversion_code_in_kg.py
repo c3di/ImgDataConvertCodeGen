e42318232319fb549f71be3375c44ea1f387eef7
Fei Chen <boxchenfei@gmail.com>
2024-03-19 20:49:55 +0100

    Fix unit tests (#10, #48)

--- tests/test_conversion_code_in_kg.py
+++ tests/test_conversion_code_in_kg.py
@@ -1,22 +1,20 @@
 import re
 
-import numpy as np
 import pytest
 import tensorflow as tf
 import torch
 
-from data_for_tests.image_data import get_test_image
-from imgdataconvertcodegen import _code_generator
+from data_for_tests.image_data import is_image_equal, get_input_image_and_expected_output
+from imgdataconvertcodegen.code_generation import ConvertCodeGenerator
+from imgdataconvertcodegen.knowledge_graph_construction import get_knowledge_graph_constructor
 
 
-def is_image_equal(image1, image2):
-    if type(image1) != type(image2):
-        return False
-    if isinstance(image1, np.ndarray):
-        return np.array_equal(image1, image2)
-    elif isinstance(image1, torch.Tensor):
-        return torch.equal(image1, image2)
-    # todo add other image types
+@pytest.fixture(scope="session")
+def code_generator():
+    constructor = get_knowledge_graph_constructor()
+    constructor.build_from_scratch()
+    code_generator = ConvertCodeGenerator(constructor.knowledge_graph)
+    return code_generator
 
 
 def assert_exec_of_conversion_code_in_edge(edge, kg):
@@ -30,31 +28,31 @@ def assert_exec_of_conversion_code_in_edge(edge, kg):
                                             f" {conversion[0]} from {source_metadata} to {target_metadata}")
     assert isinstance(conversion[1], str), (f"Expected the second element of the conversion to be a string, but got:"
                                             f" {conversion[1]} from {source_metadata} to {target_metadata}")
+    error_message = (f"conversion from\n {source_metadata} to\n {target_metadata} failed\n "
+                     f"imports: {conversion[0]}\nconversion function: \n {conversion[1]}\nfrom {edge_data.get('factory')}")
 
-    source_image = get_test_image(source_metadata)
-    target_image = get_test_image(target_metadata)
-    func_name = re.search(r'(?<=def )\w+', conversion[1]).group(0)
+    try:
+        source_image, target_image = get_input_image_and_expected_output(source_metadata, target_metadata)
+        func_name = re.search(r'(?<=def )\w+', conversion[1]).group(0)
 
-    scope = {}
-    scope.update({'source_image': source_image})
-    exec(f"""{conversion[0]}
+        scope = {}
+        scope.update({'source_image': source_image})
+        exec(f"""{conversion[0]}
 {conversion[1]}
 actual_image = {func_name}(source_image)""", scope)
-    actual_image = scope.get('actual_image')
+        actual_image = scope.get('actual_image')
+    except Exception as e:
+        raise AssertionError(f"Failed to execute conversion code from {error_message}") from e
 
-    assert is_image_equal(target_image, actual_image), (f"conversion from\n"
-                                                        f"{source_metadata} to\n"
-                                                        f"{target_metadata} failed\n"
-                                                        f"using {conversion[1]} from\n"
-                                                        f"{edge_data.get('factory')}")
+    assert is_image_equal(target_image, actual_image), f'expected {target_image}, but actual {actual_image}. {error_message}'
 
 
 def is_code_exec_on_cpu(edge):
     return edge[0]['device'] == 'cpu' and edge[1]['device'] == 'cpu'
 
 
-def test_conversion_code_exec_on_cpu():
-    kg = _code_generator.knowledge_graph
+def test_all_conversion_code_exec_on_cpu(code_generator):
+    kg = code_generator.knowledge_graph
     for edge in kg.edges:
         if is_code_exec_on_cpu(edge):
             assert_exec_of_conversion_code_in_edge(edge, kg)
@@ -81,8 +79,8 @@ def tensorflow_gpu_available():
 @pytest.mark.skipif(not pytorch_gpu_available(),
                     reason="Test skipped because PyTorch is not installed with CUDA support or"
                            " no CUDA-compatible GPU is available.")
-def test_conversion_code_exec_using_pytorch_gpu():
-    kg = _code_generator.knowledge_graph
+def test_conversion_code_exec_using_pytorch_gpu(code_generator):
+    kg = code_generator.knowledge_graph
     for edge in kg.edges:
         if is_on_gpu_as_data_repr(edge, ['torch.tensor']):
             assert_exec_of_conversion_code_in_edge(edge, kg)
@@ -91,8 +89,8 @@ def test_conversion_code_exec_using_pytorch_gpu():
 @pytest.mark.skipif(not tensorflow_gpu_available(),
                     reason="Test skipped because TensorFlow not configured for GPU acceleration or"
                            " no CUDA-compatible GPU is available.")
-def test_conversion_code_exec_using_tensorflow_gpu():
-    kg = _code_generator.knowledge_graph
+def test_conversion_code_exec_using_tensorflow_gpu(code_generator):
+    kg = code_generator.knowledge_graph
     for edge in kg.edges:
         if is_on_gpu_as_data_repr(edge, ['tf.Tensor']):
             assert_exec_of_conversion_code_in_edge(edge, kg)
@@ -101,8 +99,8 @@ def test_conversion_code_exec_using_tensorflow_gpu():
 @pytest.mark.skipif(not tensorflow_gpu_available() or not pytorch_gpu_available(),
                     reason=f"Test skipped because {"TensorFlow" if not tensorflow_gpu_available() else "Pytorch"} not"
                            f" configured for GPU acceleration or no CUDA-compatible GPU is available.")
-def test_conversion_code_exec_using_tensorflow_gpu_torch_gpu():
-    kg = _code_generator.knowledge_graph
+def test_conversion_code_exec_using_tensorflow_gpu_torch_gpu(code_generator):
+    kg = code_generator.knowledge_graph
     for edge in kg.edges:
         if is_on_gpu_as_data_repr(edge, ['tf.Tensor', 'torch.tensor']):
             assert_exec_of_conversion_code_in_edge(edge, kg)
