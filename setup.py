6ceb833907dbb8bd5a88a44df147830635924299
Luojinxuan Wang <luwa02@dfki.de>
2024-04-08 14:34:11 +0200

    set version to 0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(use_scm_version=False,
+              version="0.0.1"
+              # To specify here
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
