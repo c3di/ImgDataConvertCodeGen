c378d656424ea20f97b0d9cd4e1b1a0ab2a463ad
Fei Chen <boxchenfei@gmail.com>
2024-04-10 10:11:03 +0200

    Rename to avoid conflicts between function in __init__ and module

--- src/imgdataconvertcodegen/code_exec/__init__.py
+++ src/imgdataconvertcodegen/code_exec/__init__.py
@@ -1,2 +1,2 @@
 from .test_image_util import random_test_image_and_expected, is_image_equal
-from .time_cost import time_cost, time_cost_in_kg
+from .time_cost_measure import time_cost, time_cost_in_kg
